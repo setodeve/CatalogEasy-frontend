704ba53482c148969d85ac01e61d1ec66a63c95a
setodeve <keinick11@outlook.com>
2024-03-31 07:14:17 +0900

    feat: add yamada-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import ImageUpload from '@/components/image_upload'
+import ImageDrop from '@/components/image_drop'
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
-      <ImageUpload />
+      {/* <ImageUpload /> */}
+      <ImageDrop />
+      <ImageDrop />
+      {/* <img
+        src="http://localhost:8080/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e932d13a04edec213de2fb547737098531f77939/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-03-26%20%E5%8D%88%E5%BE%8C6.32.20.png"
+        alt=""
+      /> */}
     </main>
   )
 }
