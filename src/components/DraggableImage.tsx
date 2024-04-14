724ffeedcbd7e9b3c6622e299e0f860a356a8c7d
setodeve <keinick11@outlook.com>
2024-04-14 12:48:33 +0900

    feat: add fetch image

--- src/components/DraggableImage.tsx
+++ src/components/DraggableImage.tsx
@@ -1,7 +1,15 @@
 import { Image } from '@yamada-ui/react'
 import type { CSSProperties, LegacyRef } from 'react'
 import { useDrag } from 'react-dnd'
-const DraggableImage = ({ src, id }: { src: string; id: string }) => {
+const DraggableImage = ({
+  src,
+  id,
+  clickEvent,
+}: {
+  src: string
+  id: string
+  clickEvent?: (src: string) => void
+}) => {
   const [{ isDragging }, drag] = useDrag(() => ({
     type: 'image',
     item: { id, src },
@@ -10,31 +18,38 @@ const DraggableImage = ({ src, id }: { src: string; id: string }) => {
     }),
   }))
 
+  const styles: Record<string, CSSProperties> = {
+    item: {
+      maxWidth: '300px',
+      // border: '2px solid #ffa94d',
+      border: isDragging ? '1px dashed gray' : 'none',
+      opacity: isDragging ? 0.5 : 1,
+      // borderRadius: '8px',
+      // padding: '0.5rem',
+      // margin: '0.2rem',
+      breakInside: 'avoid',
+      cursor: 'pointer',
+      objectFit: 'cover',
+    },
+  }
+
   return (
     <Image
+      w="100px"
+      h="200px"
       _media={[{ type: 'print', css: { display: 'none' } }]}
       ref={drag as unknown as LegacyRef<HTMLImageElement>}
       src={src}
+      id={id}
       alt="Draggable"
-      style={
-        (styles.item,
-        {
-          border: isDragging ? '2px dashed gray' : 'none',
-          opacity: isDragging ? 0.5 : 1,
-        })
-      }
+      onClick={() => {
+        if (clickEvent) {
+          clickEvent(src)
+        }
+      }}
+      style={styles.item}
     />
   )
 }
 
 export default DraggableImage
-
-const styles: Record<string, CSSProperties> = {
-  item: {
-    maxWidth: '100px',
-    border: '2px solid #ffa94d',
-    borderRadius: '8px',
-    padding: '0.5rem',
-    breakInside: 'avoid',
-  },
-}
--- src/components/ImageSelection.tsx
+++ src/components/ImageSelection.tsx
@@ -1,17 +1,45 @@
 import DraggableImage from '@/components/DraggableImage'
+
+import { apiRequest } from '@/utils/apiClient'
 import { Box, VStack } from '@yamada-ui/react'
 import type { CSSProperties } from 'react'
+import { useEffect, useState } from 'react'
 const ImageSelection = () => {
+  interface Data {
+    [key: string]: string[]
+  }
+  const [images, setImages] = useState<Data | null>(null)
+  const fetchImageData = async () => {
+    return apiRequest<ImageData>('GET', `/product_images`)
+  }
+  const handleImageClick = (url: string) => {
+    window.open(url, '_blank')
+  }
+  useEffect(() => {
+    fetchImageData()
+      .then((res) => {
+        setImages(res)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [])
+
   return (
     <VStack style={styles.container} className="hideInPrint">
       <Box style={styles.grid}>
-        {Array.from({ length: 100 }).map((_, i) => (
-          <DraggableImage
-            src={`https://picsum.photos/id/${100 + i}/200/300`}
-            id={`unique-id-${i}`}
-            key={i}
-          />
-        ))}
+        {images
+          ? Object.entries(images).map(([key, urls]) =>
+              urls.map((url, i) => (
+                <DraggableImage
+                  src={url}
+                  id={key + `,${i}`}
+                  key={key + `${i}`}
+                  clickEvent={handleImageClick}
+                />
+              )),
+            )
+          : null}
       </Box>
     </VStack>
   )
@@ -24,20 +52,20 @@ const styles: Record<string, CSSProperties> = {
     backgroundColor: '#e5f2fe',
     display: 'inline-block',
     width: '50%',
-    height: '100vh',
+    // height: '100vh',
     position: 'fixed',
     top: 0,
     right: 0,
     overflow: 'scroll',
     // columnCount: 15,
-    // gridTemplateRows: 'auto',
+    gridTemplateRows: 'auto',
   },
   grid: {
-    padding: '1em',
+    padding: '0.5em',
     display: 'grid',
-    gridTemplateColumns: 'repeat(10, 1fr)',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(100px, 1fr))',
     gridTemplateRows: 'repeat(12, 1fr)',
-    gap: '15px',
-    height: '100vh',
+    // gap: '15px',
+    // height: '100vh',
   },
 }
