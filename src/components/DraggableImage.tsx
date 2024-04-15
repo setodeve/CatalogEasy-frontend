f9f56a3790539e3563bcde88be1b58727e3dd736
setodeve <keinick11@outlook.com>
2024-04-15 11:12:10 +0900

    feat: change image size

--- src/components/DraggableImage.tsx
+++ src/components/DraggableImage.tsx
@@ -20,23 +20,24 @@ const DraggableImage = ({
 
   const styles: Record<string, CSSProperties> = {
     item: {
-      maxWidth: '300px',
+      maxWidth: '350px',
       // border: '2px solid #ffa94d',
       border: isDragging ? '1px dashed gray' : 'none',
-      opacity: isDragging ? 0.5 : 1,
+      opacity: isDragging ? 0.3 : 1,
       // borderRadius: '8px',
       // padding: '0.5rem',
       // margin: '0.2rem',
       breakInside: 'avoid',
       cursor: 'pointer',
       objectFit: 'cover',
+      marginTop: '10px',
     },
   }
 
   return (
     <Image
-      w="100px"
-      h="200px"
+      w="350px"
+      h="350px"
       _media={[{ type: 'print', css: { display: 'none' } }]}
       ref={drag as unknown as LegacyRef<HTMLImageElement>}
       src={src}
