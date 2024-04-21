465481c483949fe1ab97f2f04d44efd7139fcbb6
setodeve <keinick11@outlook.com>
2024-04-22 03:27:11 +0900

    feat: add switching favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,36 @@ import type { AppProps } from 'next/app'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { AuthProvider } from '@/components/AuthContext'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <UIProvider>
-        <DndProvider backend={HTML5Backend}>
-          <Header />
-          <Component {...pageProps} />
-        </DndProvider>
-      </UIProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <title>CatalogEasy</title>
+        <link
+          rel="icon"
+          href="catalogcat_white.png"
+          media="(prefers-color-scheme: dark)"
+          sizes="any"
+          type="image/x-icon"
+        />
+        <link
+          rel="icon"
+          href="catalogcat_black.png"
+          media="(prefers-color-scheme: light)"
+          sizes="any"
+          type="image/x-icon"
+        />
+      </Head>
+      <AuthProvider>
+        <UIProvider>
+          <DndProvider backend={HTML5Backend}>
+            <Header />
+            <Component {...pageProps} />
+          </DndProvider>
+        </UIProvider>
+      </AuthProvider>
+    </>
   )
 }
