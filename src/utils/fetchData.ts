ae8c3b22d8f3694918ac32de62b2984e3a6687c2
setodeve <keinick11@outlook.com>
2024-04-24 20:12:19 +0900

    feat: add fetch user data

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,6 +1,6 @@
 import { apiRequest } from '@/utils/apiClient'
 import type { ImageSelectionData } from '@/types/image-selection'
-
+import type { UserData } from '@/types/user-data'
 export const fetchImageData = async () => {
   return apiRequest<ImageSelectionData>('GET', `/product_images`)
 }
@@ -11,6 +11,10 @@ export const fetchCatalogsData = async () => {
 
 export const fetchCatalogData = async (id) => {
   return apiRequest<string[] | null>('GET', `/catalogs/${id}`)
+}
+
+export const fetchSessionData = async () => {
+  return apiRequest<UserData>('GET', `/auth/sessions`)
 }
 
 export const uploadImageData = async (data) => {
